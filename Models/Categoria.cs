fe80ffd154da20c818a021253eef38f54cdcd7d0
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-16 21:55:58 -0300

    feat: adiciona propriedade de navegação na class Categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,6 +1,19 @@
-﻿namespace ProCatalog.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProCatalog.API.Models;
+
+public class Categoria
 {
-    public class Categoria
-    {
-    }
+    /* 
+     Anotação: 
+     -> O "ID" é a chave primária.
+     -> O Entity Framework entende automaticamente que esse campo deve ser auto-incremento no banco de dados. */
+    [Key]
+    public int Id { get; set; }
+
+    /* 
+     Anotação: 
+     -> Uso "string.Empty" para evitar que o campo venha nulo.
+     -> Em uma API, o Nome é o que identifica a categoria para o usuário. */
+    public string Nome { get; set; } = string.Empty;
 }

7045cd06e007fe023d5c4c8a25b887907a4d1296
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-17 21:54:40 -0300

    feat: adiciona metodo GET ao controller

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -1,6 +1,23 @@
-﻿namespace ProCatalog.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ProCatalog.API.Models;
+
+namespace ProCatalog.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class CategoriaController : ControllerBase
 {
-    public class CategoriaController
+
+    [HttpGet]
+    public IActionResult Get()
     {
+        var categorias = new List<Categoria>
+        {
+            new Categoria { Id = 1, Nome = "Eletrônicos" },
+            new Categoria { Id = 2, Nome = "Games" }
+        };
+
+        return Ok(categorias);
     }
 }
